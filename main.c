bb5decb61b71852ac255c5966031aada0e1d7b4a
qfc5036 <qfc5036@psu.edu>
2020-09-03 04:04:29 +0000

    l

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main(void) {
   char *temperature =readline("Enter your temperature in celsius: ");
   double newtemp = atof(temperature)* 1.8 + 32;
   double temp = atof(temperature);
-  printf("%f°in Celsius is equivalent to %f° Fahrenheit. \n", temp, newtemp);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit. \n", temp, newtemp);
   return 0;
 }
