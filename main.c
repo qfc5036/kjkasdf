f504f648118555105f310e2b3f7b1473a4d80c9d
qfc5036 <qfc5036@psu.edu>
2020-09-04 03:34:52 +0000

    k

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 // Author Qiaoxu Cui qfc5036@psu.edu 
-// Collaborator Zhili Hu zjh5625@psu.edu
-// Collaborator Shufang Huang sbh5655@psu.edu 
-// Collaborator Isabel Vera ikv5018@psu.edu
+// Collaborator: Zhili Hu zjh5625@psu.edu
+// Collaborator: Shufang Huang sbh5655@psu.edu 
+// Collaborator: Isabel Vera ikv5018@psu.edu
 
 #include <stdio.h>
 #include <stdlib.h>
